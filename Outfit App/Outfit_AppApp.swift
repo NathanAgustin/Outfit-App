ab2647f1cb55e6ac72c5be4f54e0b61d1d7fa4d9
Nathan Agustin Mac <nathan.agustin629@gmail.com>
2026-04-25 13:03:01 -0700

    Initial Commit

---
+++ Outfit App/Outfit_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outfit_AppApp.swift
+//  Outfit App
+//
+//  Created by Nathan Agustin on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct Outfit_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
